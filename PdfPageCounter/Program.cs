1feee8321762e88883551d954c6ff84149c5c382
Mojidul <mojidul31@gmail.com>
2023-05-20 11:04:46 +0600

    Add project files.

---
+++ PdfPageCounter/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+using iTextSharp.text.pdf;
+using System.Reflection.PortableExecutable;
+
+Console.WriteLine("Hello, World!");
+
+int pageCount = 0;
+String[] pdfFiles = Directory.GetFiles(@"C:\Users\Mojidul\Desktop\MT\Test\DotNet\Core\PdfPageCount\", "*.pdf");
+Console.WriteLine("{0} PDF Files in directory",pdfFiles.Length.ToString());
+for (int i = 0; i < pdfFiles.Length; i++)
+{
+    pageCount = GetTotalPageNumber(pdfFiles[i]);
+    Console.WriteLine("{0} File has {1} pages", pdfFiles[i], pageCount.ToString());
+}
+Console.ReadLine();
+
+static int GetTotalPageNumber(String filePath)
+{
+    PdfReader pdfReader = new PdfReader(filePath);
+    return pdfReader.NumberOfPages;
+}
+
+
